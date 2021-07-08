1928b390dedd9b08338bbedd0e61d1c09b2e8d11
dumi994 <dumi.9944@gmail.com>
2021-07-08 16:46:42 +0200

    create le routes

---
+++ blog/routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/characters', function (){
+    return view('characters');
+});
+Route::get('/movies', function (){
+    return view('movies');
+});
+Route::get('/tv', function (){
+    return view('tv');
+});
+Route::get('/games', function (){
+    return view('games');
+});
+Route::get('/collectibles', function (){
+    return view('collectibles');
+});
+Route::get('/videos', function (){
+    return view('videos');
+});
+Route::get('/fans', function (){
+    return view('fans');
+});
+Route::get('/news', function (){
+    return view('news');
+});
+Route::get('/shop', function (){
+    return view('shop');
+});
