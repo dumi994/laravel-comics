bde2db5c567a4d2bbd7fbc0c5d2f7bc0be7a2ebd
dumi994 <dumi.9944@gmail.com>
2021-07-09 19:55:25 +0200

    aggiunti elements

--- blog/resources/views/partials/main.blade.php
+++ blog/resources/views/partials/main.blade.php
@@ -1,3 +1,22 @@
 <main>
-    
+    <div class="elements">
+        <div>
+            <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+            <span>DIGITAL COMICS</span>
+        </div>
+        <div>
+            <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="">
+            <span>DC MERCHANDISE</span>
+        </div>
+        <div>
+            <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="">
+            <span>SUBSCRIPTION</span>
+        </div>
+        <div>
+            <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="">
+            <span>COMICS SHOP LOCATOR</span>
+
+        </div>
+        
+    </div>
 </main>
